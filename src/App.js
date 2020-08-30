0001612a4700b074c0cb30188c36b1772abcc6a1
pherwerz <lordrisk101@gmail.com>
2020-08-30 14:17:14 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import Layout from './containers/layout/layout';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default App;
